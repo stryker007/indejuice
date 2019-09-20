ab3278e885b894078b1f340f80dc7919ac40d226
unknown <hakeem@javaid.co>
2019-09-20 18:37:21 +0100

    Test Files

---
+++ task-b/code.php
@@ -0,0 +1,26 @@
+<?php
+
+    /* 
+     *  Function    : generateSentence()
+     * 
+     *  Description : Generate a sentence listing out all items provided in an array.
+     *  
+     *  Parameters  : (string)               start         - The start of the sentence
+     *                (array[string])        sentence      - The list of items
+     *                (string/array[string]) append        - (optional) item types to be appended to each item
+     * 
+     *  Return      : string
+     * 
+     *  Example     : See run.php for input/output examples
+     *  
+     *  Test        : Execute run.php to test your implementation.
+     *                (In console, simply run the command 'php run.php')
+     * 
+     */
+
+    
+    function generateSentence(){
+        
+
+
+    }
